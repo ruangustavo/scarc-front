75d51ea9ac309325a38544b78dd3398fdeb15f4e
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2023-12-10 11:16:23 -0300

    feat: add sidebar navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import { SidebarNav } from '@/components/sidebar-nav'
+import { LayoutGrid } from 'lucide-react'
+
+const sidebarNavItems = [
+  {
+    icon: <LayoutGrid width={16} height={16} />,
+    title: 'Início',
+    href: '/',
+  },
+]
+
 export default function Home() {
   return (
-    <main>
-      <p>Olá, mundo!</p>
-    </main>
+    <div className="flex-1 grid grid-cols-[200px_1fr] gap-12">
+      <div className="hidden md:block">
+        <aside>
+          <SidebarNav items={sidebarNavItems} />
+        </aside>
+      </div>
+      <main>
+        <p>Aqui está o conteúdo</p>
+      </main>
+    </div>
   )
 }
